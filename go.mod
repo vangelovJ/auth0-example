3d26dd6d2517f674a21585d01b869bf877a50bbb
vangelovj <vangelov@jboxers.com>
2021-10-21 07:46:05 -0700

    Add user changes

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/vangelovj/auth0-example
+module 01-Login
 
 go 1.16
 
@@ -7,5 +7,6 @@ require (
 	github.com/gin-contrib/sessions v0.0.3
 	github.com/gin-gonic/gin v1.7.4
 	github.com/joho/godotenv v1.4.0
+	github.com/vangelovj/auth0-example v0.0.0-20211021142121-d5b04a7384bf
 	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1
 )
--- web/app/user/user.go
+++ web/app/user/user.go
@@ -1,37 +1,54 @@
 package user
 
 import (
-	"io/ioutil"
+	"github.com/gin-contrib/sessions"
 	"net/http"
 
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-gonic/gin"
-	"os"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"encoding/json"
 )
 
+type User struct{
+	Name string `json:"name"`
+}
 // Handler for our logged-in user page.
 func Handler(ctx *gin.Context) {
 	session := sessions.Default(ctx)
 	profile := session.Get("profile")
 
-	ctx.HTML(http.StatusOK, "user.html", profile)
+	//ctx.HTML(http.StatusOK, "user.html", profile)
+	v := CheckRole(profile)
+	fmt.Println(v)
 	ctx.Redirect(http.StatusTemporaryRedirect, "http://192.168.163.132:8080/test1/")
 }
 
 
-func CheckRole(user string){
-	url := "https://"+os.Getenv("AUTH0_DOMAIN")+"/api/v2/users/"+user+"/roles"
+func CheckRole(profile interface{}) string{
+	//fmt.Println(profile)
 
-	req, _ := http.NewRequest("GET", url, nil)
+	profileData,err := json.Marshal(profile)
+	if err != nil{
+		fmt.Errorf(err.Error())
+	}
+	var u User
+	err = json.Unmarshal(profileData,&u)
+	if err != nil{
+		fmt.Errorf(err.Error())
+	}
+	fmt.Println(u.Name)
+	return u.Name
+	//url := "https://"+os.Getenv("AUTH0_DOMAIN")+"/api/v2/users/"+profile["name"]+"/roles"
 
-	req.Header.Add("authorization", "Bearer "+os.Getenv("MGMT_API_ACCESS_TOKEN")+"")
+	//req, _ := http.NewRequest("GET", url, nil)
 
-	res, _ := http.DefaultClient.Do(req)
+	//req.Header.Add("authorization", "Bearer "+os.Getenv("MGMT_API_ACCESS_TOKEN")+"")
 
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
+	//res, _ := http.DefaultClient.Do(req)
 
-	fmt.Println(res)
-	fmt.Println(string(body))
+	//defer res.Body.Close()
+	//body, _ := ioutil.ReadAll(res.Body)
+
+	//fmt.Println(res)
+	//fmt.Println(string(body))
 }
